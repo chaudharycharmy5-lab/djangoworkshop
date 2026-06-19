f5f3abddffc371e9b45d1a7c650e7ead90d5807f
Charmy Chaudhary <chaudharycharmy5@gmail.com>
2026-06-19 22:45:05 +0530

    Update settings.py

--- crud_demo/crud_demo/settings.py
+++ crud_demo/crud_demo/settings.py
@@ -20,9 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9t21z0o-s0yp2&zb8(rh)o45*d(pwicp_n9t-52eiq#1q*r8!g'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -134,8 +131,8 @@ EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_USE_TLS=True
 EMAIL_PORT=587
-EMAIL_HOST_USER='ccharmy22@gmail.com'
-EMAIL_HOST_PASSWORD='emir fmvw zcnm kizr'
+EMAIL_HOST_USER=''
+EMAIL_HOST_PASSWORD=''
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
