36aec3244a733e5d4a9064abb0ec7b68cb1c7091
Charmy Chaudhary <chaudharycharmy5@gmail.com>
2026-06-19 21:18:09 +0530

    Update settings.py

--- sessiondemo/sessiondemo/settings.py
+++ sessiondemo/sessiondemo/settings.py
@@ -20,9 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xuqzja=g+vpbsu^f#44g^x%7_u!)@b)bv4#foy9*8o!vpc^08f'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
