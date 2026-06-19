690b7da4326e8ad28066b13bf92a9ad76586b8e9
Charmy Chaudhary <chaudharycharmy5@gmail.com>
2026-06-19 15:21:23 +0530

    Add files via upload

--- crud_demo/crud/urls.py
+++ crud_demo/crud/urls.py
@@ -4,4 +4,6 @@ from django.urls import path
 urlpatterns = [
       path('studentform', views.addstudent),
       path('add-student',views.addstudentprocess),
+      path('display-student',views.displaystudent),
+      path('delete-student/<int:id>',views.deletestudent)
 ]
--- crud_demo/crud/views.py
+++ crud_demo/crud/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from . models import Student
 
@@ -13,3 +13,11 @@ def addstudentprocess(request):
     txt4=request.POST['txt4']
     Student.objects.create(name=txt1,mobile=txt2,email=txt3,address=txt4)
     return HttpResponse('Thank You!')
+
+def displaystudent(request):
+    mystudentlist=Student.objects.all()
+    return render(request,'display.html',{'mydata':mystudentlist})
+
+def deletestudent(request,id):
+    Student.objects.get(id=id).delete()
+    return redirect(displaystudent)
