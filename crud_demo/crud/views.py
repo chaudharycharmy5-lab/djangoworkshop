1e6b4056ed964c071ddf28606fa0cb425586d868
Charmy Chaudhary <chaudharycharmy5@gmail.com>
2026-06-19 13:21:03 +0530

    CRUD Concept
    Day 5

---
+++ crud_demo/crud/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from . models import Student
+
+# Create your views here.
+def addstudent(request):
+    return render(request,'studentform.html')
+
+def addstudentprocess(request):
+    txt1=request.POST['txt1']
+    txt2=request.POST['txt2']
+    txt3=request.POST['txt3']
+    txt4=request.POST['txt4']
+    Student.objects.create(name=txt1,mobile=txt2,email=txt3,address=txt4)
+    return HttpResponse('Thank You!')
